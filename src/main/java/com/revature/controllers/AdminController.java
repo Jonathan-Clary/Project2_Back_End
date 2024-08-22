84d9d486c3f187044af1ccfc00f88243c5c4224c
WKNick <150284814+WKNick@users.noreply.github.com>
2024-08-22 16:37:33 -0400

    Admin create and get functionality
    Not complete but has most functionality mostly needs some checks and better error messages

--- src/main/java/com/revature/controllers/AdminController.java
+++ src/main/java/com/revature/controllers/AdminController.java
@@ -1,14 +1,16 @@
 package com.revature.controllers;
 
 import com.revature.DTOs.OutgoingAdminDTO;
+import com.revature.exceptions.CustomException;
+import com.revature.models.Admin;
 import com.revature.services.AdminService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/admin")
@@ -18,6 +20,10 @@ public class AdminController {
     //Service Variable(s)
     private AdminService as;
 
+    /* TODO: uncomment when admin authcontroller is made
+    @Autowired
+    AdminAuthController authController;
+    */
     //Constructor
     @Autowired
     public AdminController(AdminService as) {
@@ -29,4 +35,37 @@ public class AdminController {
     public List<OutgoingAdminDTO> getAllAdmins() {
         return as.getAllAdmins();
     }
+
+    @PostMapping("/register")
+    public ResponseEntity<Admin> createAdmin(@RequestBody @Valid Admin admin)throws CustomException {
+        try{
+            Admin returningAdmin =  as.createAdmin(admin);
+            return ResponseEntity.status(201).body(returningAdmin);
+        }catch(Exception e){
+            return ResponseEntity.status(400).body(null);
+        }
+    }
+
+    @PatchMapping
+    public ResponseEntity<Object> updateLoggedInAdminProfile(@RequestBody Map<String,String> newAdmin) throws CustomException {
+        try{
+            Admin admin = as.updateAdminById(loggedInAdminId(), newAdmin);
+            return ResponseEntity.ok(admin);
+        }catch(Exception e){
+            return ResponseEntity.status(400).body(null);
+        }
+    }
+
+    private int loggedInAdminId() throws CustomException {
+
+        Admin authAdmin = null;// authController.getAuthenticatedAdmin(); TODO: uncomment when adminauthcontroller is made
+        if(authAdmin != null){
+            return authAdmin.getAdminId();
+        }
+        return 0;
+
+    }
+
+
+
 }
