a9dc6a666fbe5c24d3bc9bd38fc4f1b21dc4c32d
Abdeljabbar Elassali <115737234+asly-97@users.noreply.github.com>
2024-08-22 16:39:47 -0400

    Set up AdminSecurityConfig

--- src/main/java/com/revature/config/UserSecurityConfig.java
+++ src/main/java/com/revature/config/UserSecurityConfig.java
@@ -9,19 +9,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class UserSecurityConfig {
 
     @Autowired
     JwtRequestFilter jwtRequestFilter;
