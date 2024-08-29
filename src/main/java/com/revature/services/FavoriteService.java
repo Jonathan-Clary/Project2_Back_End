96b7da35498454da16a2b4c5c0f133f8bfb5feb1
WKNick <nick.spaceheater@gmail.com>
2024-08-29 16:41:30 -0400

    Fixed FavoriteServiceTest

--- src/main/java/com/revature/services/FavoriteService.java
+++ src/main/java/com/revature/services/FavoriteService.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 @Service
 public class FavoriteService {
