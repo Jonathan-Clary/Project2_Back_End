e0b14deee5ee676d341640ccc52c6c0c82930488
Randy Li <randyliisawesome@gmail.com>
2024-08-28 23:45:11 -0400

    finished unit tests for StayServiceTest.java and added unit tests for UserServiceTest.java

--- src/test/java/com/revature/TravelPlanner/StayServiceTest.java
+++ src/test/java/com/revature/TravelPlanner/StayServiceTest.java
@@ -3,6 +3,7 @@ package com.revature.TravelPlanner;
 import com.revature.DAOs.StayDAO;
 import com.revature.DTOs.HotelDTO;
 import com.revature.exceptions.CustomException;
+import com.revature.exceptions.HotelNotFoundException;
 import com.revature.exceptions.InvalidIDException;
 import com.revature.exceptions.StayNotFoundException;
 import com.revature.models.User;
@@ -84,5 +85,78 @@ public class StayServiceTest {
             stayService.createStay(stay);
         });
         verify(stayDAO, never()).save(any(Stay.class));
+    }
+
+    @Test
+    void testDeleteStay_Successful() {
+
+        stayService.deleteStayById(UUID_TEST_1);
+        verify(stayDAO, times(1)).deleteById(UUID_TEST_1);
+    }
+
+    @Test
+    void testGetStay_Successful() throws CustomException {
+
+        Stay stay = new Stay();
+
+        when(stayDAO.findById(UUID_TEST_1)).thenReturn(Optional.of(stay));
+
+        Stay result = stayService.getStayById(UUID_TEST_1);
+
+        assertNotNull(result);
+        assertEquals(stay, result);
+
+        verify(stayDAO, times(1)).findById(UUID_TEST_1);
+    }
+
+    @Test
+    void testGetStay_StayNotFound() {
+        when(stayDAO.findById(UUID_TEST_1)).thenReturn(Optional.empty());
+
+        assertThrows(StayNotFoundException.class, () -> {
+            stayService.getStayById(UUID_TEST_1);
+        });
+
+        verify(stayDAO, times(1)).findById(UUID_TEST_1);
+    }
+
+    @Test
+    void testUpdateStay_Successful() throws CustomException {
+
+        Stay stay = new Stay();
+        stay.setStayId(UUID_TEST_1);
+        Hotel hotel = new Hotel();
+        hotel.setHotelId(UUID_TEST_1);
+
+        Map<String, String> stayFieldValues = new HashMap<>();
+        stayFieldValues.put("hotelId", UUID_TEST_1.toString());
+
+        when(stayDAO.findById(UUID_TEST_1)).thenReturn(Optional.of(stay));
+        when(hotelService.getHotelById(UUID_TEST_1)).thenReturn(hotel);
+        when(stayDAO.save(any(Stay.class))).thenReturn(stay);
+
+        Stay updatedStay = stayService.updateStay(UUID_TEST_1, stayFieldValues);
+
+        assertNotNull(updatedStay);
+        assertEquals(hotel, updatedStay.getHotel());
+
+        verify(hotelService, times(1)).getHotelById(UUID_TEST_1);
+        verify(stayDAO, times(1)).save(stay);
+    }
+
+    @Test
+    void testUpdateStay_StayNotFound() {
+
+        Hotel hotel = new Hotel();
+        hotel.setHotelId(UUID_TEST_1);
+
+        Map<String, String> stayFieldValues = new HashMap<>();
+        stayFieldValues.put("hotelId", UUID_TEST_1.toString());
+
+        when(stayDAO.findById(UUID_TEST_1)).thenReturn(Optional.empty());
+
+        assertThrows(StayNotFoundException.class, () -> {
+            stayService.updateStay(UUID_TEST_1, stayFieldValues);
+        });
     }
 }
